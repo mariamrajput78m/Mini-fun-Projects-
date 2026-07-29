d74f9e4acdddbc7c8b1a9f040410e558dfd9ffc9
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-29 07:40:35 +0000

    chnages in script

---
+++ script.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll("Button");
+
+btns.forEach(function (buttons) {
+    console.log(buttons);
+    buttons.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id == "grey") {
+            body.style.background = e.target.id;
+        }
+        else if (e.target.id == "yellow") {
+            body.style.background = e.target.id;
+        }
+        else if (e.target.id == "pink") {
+            body.style.background = e.target.id;
+        }
+        else {
+            body.style.background = e.target.id;
+        }
+    })
+})

77dd561fc2624695b68048c943496f10b8ca834a
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-31 19:56:20 +0000

    script js working

--- 02-BMI-Calculator/script.js
+++ 02-BMI-Calculator/script.js
@@ -1,1 +1,17 @@
 const form = doucment.querySelector("form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#result');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please give valid height ${height}`;
+    }
+    if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please give valid height ${weight}`;
+    }
+
+})
